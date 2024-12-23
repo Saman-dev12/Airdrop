6370c8245951a13ff9fff0b01c2116d4a8368f55
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2024-12-23 21:39:38 +0530

    Added balance

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,24 @@
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import React from 'react';
+import { PublicKey } from '@solana/web3.js';
+import React, { useEffect } from 'react';
 
 const Navbar: React.FC = () => {
+    const [balance, setBalance] = React.useState<number>(0);
+    const {connection} = useConnection()
+    const wallet = useWallet()
+    const getBalance = async () => {
+            
+        if (wallet.publicKey) {
+            const balance = await connection.getBalance(new PublicKey(wallet.publicKey));
+            setBalance(balance / 1000000000);
+        }else{
+            setBalance(0)
+        }
+    }
+    useEffect(() => {   
+        getBalance();
+    }, [wallet.publicKey]); 
     return (
         <div className="w-full">
             <nav className="bg-blue-600 p-4">
@@ -10,9 +27,12 @@ const Navbar: React.FC = () => {
                         Airdrop Application
                     </div>
                     <div className="space-x-4 flex">
-
+                        {wallet.publicKey && (<div className="bg-purple-800 flex items-center justify-center text-white px-4 py-2 rounded">
+                            Balance: {balance.toFixed(2)} SOL
+                        </div>)}
+                        
                        <div><WalletMultiButton /></div>
-                       <div><WalletDisconnectButton /></div>
+                       <div><WalletDisconnectButton onClick={getBalance} /></div>
                        
                     </div>
                 </div>

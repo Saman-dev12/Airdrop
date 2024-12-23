6ef3da8823d641d63be87634deab5bbbc0ad3f53
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2024-12-23 19:34:22 +0530

    Added airdrop functionality

--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
 import Navbar from "./Navbar";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { clusterApiUrl, Connection ,LAMPORTS_PER_SOL} from "@solana/web3.js";
 
 function Airdrop() {
-    const [address, setAddress] = useState<string>('')
+    const [amount, setAmount] = useState<number>(0)
+    const wallet = useWallet()
+    
     const addSol = () => {
-        if(address.trim() === '') {
-            alert('Please enter a valid address')
+        if(amount <= 0) {
+            alert('Please enter a valid amount')
             return
         }
-        // Add sol here
-        console.log("Adding sol to address: ", address)
+        if(!wallet.publicKey) {
+            alert('Please connect your wallet')
+            return
+        }
+        const connection =  new Connection(clusterApiUrl('devnet'), 'confirmed')
+        connection.requestAirdrop(wallet.publicKey, amount * LAMPORTS_PER_SOL).then(() => {
+            alert('Airdrop successful')
+            }).catch((error) => {
+                console.log(error)
+                alert('Airdrop failed')
+            })
     }
 
   return (
@@ -22,11 +35,16 @@ function Airdrop() {
           Enter your Solana wallet address below to receive free SOL tokens!
         </p>
         <input
-            onChange={(e) => setAddress(e.target.value)}
-          type="text"
+            onChange={(e) => setAmount(Number(e.target.value))}
+          type="number"
+          min={1}
+          step={0.01}
+          max={10}
+          value={amount}
           placeholder="Enter your wallet address"
           className="w-full p-3 mb-4 border border-gray-300 rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-red-500"
         />
+        
         <button onClick={addSol} className="bg-red-600 text-white py-2 px-4 rounded-md text-lg hover:bg-red-700 transition">
           Claim Airdrop
         </button>
